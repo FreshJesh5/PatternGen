92d4f3b528c7e2126b5c60d47fe64f1d957dcdad
Josh Chen <josh.chen4a@gmail.com>
2017-07-12 09:19:28 +0800

    Working Copy

---
+++ app/src/main/java/com/auo/pg/pattern/optical/gradient/OpticalGrad1Pattern.java
@@ -0,0 +1,10 @@
+package com.auo.pg.pattern.optical.gradient;
+
+import com.auo.pg.pattern.optical.OpticalPattern;
+
+/**
+ * Created by joshc on 7/12/2017.
+ */
+
+public class OpticalGrad1Pattern extends OpticalPattern {
+}
